72bd29fb789ad8ccf399b547072b5469e2e4cac9
bwegrzyn0 <70061592+bwegrzyn0@users.noreply.github.com>
2023-12-01 16:50:16 +0100

    Usunąłem klasę MathsTest.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,5 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        MathsTest mt = new MathsTest();
-        mt.test(); // testowanie
     }
 }
