e2a15c36a5e1c6468aa35d3e7725090734b1be8b
bwegrzyn0 <70061592+bwegrzyn0@users.noreply.github.com>
2023-12-04 20:52:40 +0100

    Kostka

--- src/main/RendererTest.java
+++ src/main/RendererTest.java
@@ -23,22 +23,41 @@ public class RendererTest extends Renderer {
     public RendererTest(Vector2 dimensions, Camera camera) {
         super(dimensions, camera);
         triangles = new Triangles();
-        Vector3 verticies[] = new Vector3[]{new Vector3(0, 0, 0),
-                new Vector3(1, 0, 0), new Vector3(0, 1, 0)};
-        triangles.triangles.add(new Triangle(verticies, this, camera));
-        verticies = new Vector3[]{new Vector3(0, 1, 0),
-                new Vector3(1, 1, 0), new Vector3(1, 0, 0)};
-        triangles.triangles.add(new Triangle(verticies, this, camera));
-        verticies = new Vector3[]{new Vector3(0, 0, 3),
-                new Vector3(1, 0, 2), new Vector3(0, 1, 3)};
-        Triangle t1 = new Triangle(verticies, this, camera);
-        t1.color = Color.GREEN;
-        triangles.triangles.add(t1);
-        verticies = new Vector3[]{new Vector3(0, 1, 3),
-                new Vector3(1, 1, 2), new Vector3(1, 0, 3)};
-        Triangle t2 = new Triangle(verticies, this, camera);
-        t2.color = Color.GREEN;
-        triangles.triangles.add(t2);
+        Vector3[] vertices = new Vector3[]{
+                new Vector3(0, 0, 0),
+                new Vector3(1, 0, 0),
+                new Vector3(1, 1, 0),
+                new Vector3(0, 1, 0),
+                new Vector3(0, 0, 1),
+                new Vector3(1, 0, 1),
+                new Vector3(1, 1, 1),
+                new Vector3(0, 1, 1),
+        };
+
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[0], vertices[1], vertices[2]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[0], vertices[2], vertices[3]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[4], vertices[5], vertices[6]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[4], vertices[6], vertices[7]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[0], vertices[3], vertices[7]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[0], vertices[7], vertices[4]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[1], vertices[5], vertices[6]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[1], vertices[6], vertices[2]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[0], vertices[4], vertices[5]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[0], vertices[5], vertices[1]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[3], vertices[2], vertices[6]},
+                camera.renderer, camera));
+        triangles.triangles.add(new Triangle(new Vector3[]{vertices[3], vertices[6], vertices[7]},
+                camera.renderer, camera));
     }
 
     public void render(Graphics2D graphics) {
